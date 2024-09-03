4618fae990e6e021bb0a7d58a6781f018dc72a3f
juanmarin444 <juanmarin444@yahoo.com>
2024-09-02 20:41:19 -0600

    changed form submit function

--- src/app/sections/Contact/Contact.js
+++ src/app/sections/Contact/Contact.js
@@ -1,19 +1,36 @@
 'use client'
+import { useState } from 'react';
+
 import { Montserrat } from "next/font/google";
 
 const montserrat = Montserrat({ subsets: ["latin"] })
 
 export default function Contact () {
+  const [status, setStatus] = useState(null);
+  const [error, setError] = useState(null);
 
   const handleFormSubmit = async (event) => {
     event.preventDefault();
-    const formData = new FormData(event.target);
-    await fetch('/__forms.html', {
+    try {
+      setStatus('pending');
+      setError(null);
+      const myForm = event.target;
+      const formData = new FormData(myForm);
+      const res = await fetch('/__forms.html', {
         method: 'POST',
-        headers: {'Content-Type': 'application/x-www-form-urlencoded'},
+        headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
         body: new URLSearchParams(formData).toString()
-    });
-    // Success & error handling should come here
+      });
+      if (res.status === 200) {
+        setStatus('ok');
+      } else {
+        setStatus('error');
+        setError(`${res.status} ${res.statusText}`);
+      }
+    } catch (e) {
+      setStatus('error');
+      setError(`${e}`);
+    }
   };
 
   return (
@@ -40,9 +57,56 @@ export default function Contact () {
               <div data-netlify-recaptcha=""></div>
             </div> */}
             <input className="border-b-2 border-neutral-100 dark:border-neutral-900 hover:border-neutral-900 dark:hover:text-neutral-50 dark:hover:border-neutral-100 cursor-pointer" type="submit" value="SEND" />
+            {status === 'ok' && (
+              <div className="alert alert-success">
+                <SuccessIcon />
+                Submitted!
+              </div>
+            )}
+            {status === 'error' && (
+              <div className="alert alert-error">
+                <ErrorIcon />
+                {error}
+              </div>
+            )}
           </form>
         </div>
       </div>
     </div>
   )
+}
+
+function SuccessIcon() {
+  return (
+      <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="stroke-current shrink-0 h-6 w-6"
+          fill="none"
+          viewBox="0 0 24 24"
+      >
+          <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"
+          />
+      </svg>
+  );
+}
+function ErrorIcon(success) {
+  return (
+      <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="stroke-current shrink-0 h-6 w-6"
+          fill="none"
+          viewBox="0 0 24 24"
+      >
+          <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"
+          />
+      </svg>
+  );
 }
